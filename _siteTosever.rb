d517d90af0eb895678d7edc524cb4d0efe177604
Simon-Bakken-Jantasuk <94875981+Simon-Bakken-Jantasuk@users.noreply.github.com>
2023-06-11 15:14:47 +0200

    Delete _siteTosever.rb

--- _siteTosever.rb
+++
@@ -1,2 +0,0 @@
-system("scp -r #{Dir.home}/Programmering/Javascript/react/blog/_site/*  root@178.79.176.33:/var/www/nettside/")
-system("scp -r #{Dir.home}/Programmering/Javascript/react/blog/_site/*  root@178.79.176.33:/var/www/nettside/")
